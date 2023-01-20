dcb2ed919624d0ecb757e1ab19ee29e9ad96f23f
Timothy jan <jan.timothy@gmail.com>
2023-01-19 16:42:22 -0800

    Finished 5-Player Controller

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Java Pong Game
 3-Handling User Input
 <ul>
     <li>Create new class <code>KL</code> to implement KeyListener. Create methods <code>keyPressed</code>, <code>keyReleased</code> and <code>isKeyPressed</code>.</li>
-    <li>Create new KL <code>keyListener</code> in Window class and use using <code>this.addKeyListener(keyListener)</code> in <code>public Window</code>.</li>
+    <li>Initialize new KL <code>keyListener</code> in Window class and use using <code>this.addKeyListener(keyListener)</code> in <code>public Window</code>.</li>
     <li>Add <code>keyListener</code> to listen for <code>KeyEvents</code> in <code>update</code> method.</li>
 </ul>
 
@@ -28,13 +28,30 @@ Java Pong Game
     <li>Create paddles and ball on the screen and will use this to detect collisions.</li>
     <li>Create class <code>Rect</code> for paddle and ball.</li>
     <li>Create new Constants for width/height/color for paddle/ball and padding.</li>
-    <li>Create playerOne, ai, and ball as new <code>Rects</code> in <code>public Window</code>. Draw them in <code>update</code> method.</li>
+    <li>Initialize playerOne, ai, and ball as new <code>Rects</code> in <code>public Window</code>. Draw them in <code>update</code> method.</li>
 </ul>
 
 5-Making the Player Controller
 <ul>
-    <li></li>
+    <li>Implement the player controller to move the player.</li>
+    <li>Create new class <code>PlayerController</code>.</li>
+    <li>Initialize <code>PlayerController</code> in <code>Window</code>.</li>
+    <li>For smoother gameplay:
+        <ul>
+            <li>Changed all int values to double values.</li>
+            <li>In <code>update</code> method, create an image <code>dbImage</code> for double buffer image. Then create a Graphics <code>dbg</code> for double buffer graphics.</li>
+            <li>We will draw everything to this dbg which will not be displayed to the user.</li>
+            <li>Then we're going to return this dbg to our <code>draw</code> method and draw the image to the screen. </li>
+            <li>This way everything gets drawn incrementally on an off screen and then we swap buffers and show this buffer to the user.</li>
+        </ul>
+    </li>
+    <li>Create new constants <code>TOOLBAR_HEIGHT</code> and <code>INSETS_BOTTOM</code> to prevent paddles going off screen. </li>
+    <li>Create new constant <code>PADDLE_SPEED</code> for use in paddle movement.</li>
 </ul>
 
-
-
+6-AI Controller
+<ul>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
--- src/Window.java
+++ src/Window.java
@@ -1,13 +1,15 @@
 import javax.swing.JFrame;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
-import java.awt.event.KeyEvent;
+import java.awt.Image;
 
 public class Window extends JFrame implements Runnable {
 
-    Graphics2D g2;
-    KL keyListener = new KL();
-    Rect playerOne, ai, ball;
+    public Graphics2D g2;
+    public KL keyListener = new KL();
+    public Rect playerOne, ai, ball;
+    public PlayerController playerController;
 
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
@@ -16,30 +18,43 @@ public class Window extends JFrame implements Runnable {
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.addKeyListener(keyListener);
+
+        Constants.TOOLBAR_HEIGHT = this.getInsets().top;
+        Constants.INSETS_BOTTOM = this.getInsets().bottom;
+
         g2 = (Graphics2D)this.getGraphics();
 
         playerOne = new Rect(Constants.HZ_PADDING,40,Constants.PADDLE_WIDTH,Constants.PADDLE_HEIGHT,Constants.PADDLE_COLOR);
+        playerController = new PlayerController(playerOne, keyListener);
+
         ai = new Rect(Constants.SCREEN_WIDTH-Constants.PADDLE_WIDTH-Constants.HZ_PADDING,40,Constants.PADDLE_WIDTH,Constants.PADDLE_HEIGHT,Constants.PADDLE_COLOR);
         ball = new Rect(Constants.SCREEN_WIDTH/2,Constants.SCREEN_HEIGHT/2,Constants.BALL_WIDTH,Constants.BALL_WIDTH,Constants.BALL_COLOR);
     }
 
     public void update(double dt) {
+        /*Draw to double buffer graphics, not displayed to user*/
+        Image dbImage = createImage(getWidth(), getHeight());
+        Graphics dbg = dbImage.getGraphics();
+        /*Swap buffers and draw image displayed to user */
+        this.draw(dbg);
+        g2.drawImage(dbImage, 0, 0,this);
+
+        playerController.update(dt);
+
+        /*Check fps*/
+//        System.out.println(dt + "seconds passed since the last frame");
+//        System.out.println(1/dt + " fps");
+    }
+
+    public void draw(Graphics g) {
+        /*Draws graphics to screen displayed to user*/
+        Graphics2D g2 = (Graphics2D)g;
         g2.setColor(Color.BLACK);
         g2.fillRect(0,0,Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
 
         playerOne.draw(g2);
         ai.draw(g2);
         ball.draw(g2);
-
-//        if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
-//            System.out.println("The user is pressing the UP arrow");
-//        } else if (keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
-//            System.out.println("The user is pressing the DOWN arrow");
-//        }
-
-        /*Check fps*/
-//        System.out.println(dt + "seconds passed since the last frame");
-//        System.out.println(1/dt + " fps");
     }
 
     public void run() {
@@ -52,10 +67,9 @@ public class Window extends JFrame implements Runnable {
                 update(deltaTime);
 
                 /*Check fps*/
-                try{
-                    Thread.sleep(30);
-                } catch(Exception e) {}
-
+//                try{
+//                    Thread.sleep(30);
+//                } catch(Exception e) {}
             }
         }
     }
